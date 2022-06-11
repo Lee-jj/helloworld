46be766536fe6bb208647c4b6f3a7490aa6c8614
Leejj <Leejj@Leejj.com>
2022-06-11 10:41:30 +0800

    first commit

---
+++ helloworld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << "hello world !" << endl;
+
+    system("pause");
+
+    return 0;
+}
